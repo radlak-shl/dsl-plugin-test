b7e698ff7554f421fd68f6dca358c98bb8e26f6f
Rohit Adlakha <Rohit.Adlakha@shl.com>
2025-08-10 18:09:49 +0000

    cicd-pipeline

---
+++ dsl/seed.groovy
@@ -0,0 +1,25 @@
+import groovy.yaml.YamlSlurper
+
+def ciRepoUrl = 'https://github.com/<your-org-or-user>/ci-ecs-pipelines.git'
+def ciBranch  = 'main'
+
+// Read apps list from CI repo (seed job must check this out)
+def apps = new YamlSlurper().parseText(readFileFromWorkspace('ci/apps.yaml'))
+
+apps.each { app ->
+    pipelineJob(app.name) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote { url(ciRepoUrl) }
+                        branch(ciBranch)
+                    }
+                }
+                scriptPath('Jenkinsfile.ecs')
+                lightweight(true)
+            }
+        }
+    }
+}
+
